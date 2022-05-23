48208ac389c14614f7541d545567efe9788542c2
Malva37 <irynapysanko@gmail.com>
2022-05-23 18:40:19 +0300

    implemented style panel:font family, font size

--- script.js
+++ script.js
@@ -1,16 +1,68 @@
-$(function(){
+$(function () {
+let fontStyleLinks = $('.fontStyleLink');
+let fontSizeLinks = $('.fontSizeLink');
+
+$('.fontStyleBurger').click(function () {
+    $('.fontStyleMenu, .fontStyleBurger').toggleClass('active');
+})
+
+$('.fontSizeBurger').click(function () {
+    $('.fontSizeMenu, .fontSizeBurger').toggleClass('active');
+})
 
 
 
 
+$('.fontStyleLink').each(function (index, element) {
+
+    $(element).click(() => {
+        text = $(this).text();
+        $('p').css({
+            fontFamily: text
+        });
+        $('.fontStyleMenu, .fontStyleBurger').toggleClass('active');
+    })
 
 
 
-
-
-
-
-
+    })
 
     
+    $('.fontSizeLink').each(function (index, element) {
+        $(element).click(() => {
+            size = $(this).text();
+            $('p').css({
+                fontSize: size
+            });
+            $('.fontSizeMenu, .fontSizeBurger').toggleClass('active');
+        })
+
+
+
 })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+})
+
+
+
+
+
+
+
+
+
+
+
